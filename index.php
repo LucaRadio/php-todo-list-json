ab3520488e4481896e1e5c16881dc35a8cd31895
LucaRadio <luca.1407@hotmail.it>
2022-12-22 17:37:30 +0100

    Complete base exercise

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+
+
+?>
+    
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,19 +12,49 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.2.2/axios.min.js" integrity="sha512-QTnb9BQkG4fBYIt9JGvYmxPpd6TBeKp6lsUrtiVQsrJ9sb33Bn9s0wMQO9qVBFbPX3xHRAsBHvXlcsrnJjExjg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+    <link rel="stylesheet" href="css/style.css">
 </head>
-<body>
+<body class="bg-dark">
     <div id="app">
-
-
-
+    <div class="container text-center">
+        <h1 class="text-warning my-4">Programming language to learn</h1>
+        <p class="fs-4 text-white mb-5">
+            There's a ToDoList where you can follow your learning about programming language
+        </p>
+        <div class="row flex-column align-items-center">
+            <div class="bg-white col-5 mb-3">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item text-start p-0 d-flex align-items-center" v-for="singleLang in ListPL">
+                        <div class="img-container me-3">
+                            <img :src="singleLang.logo" alt="">
+                        </div>
+                        <div class="input-group my-2 d-flex align-items-center">
+                            <span class="me-auto">{{singleLang.name}}</span>
+                            <button class="input-group-text bg-danger" id="basic-addon2"><i class=" text-white fa-solid fa-trash"></i></button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-5">
+                <form @submit.prevent="insertNew" method="POST" class="input-group" action="./api/newPLang.php">
+                    <input require type="text" class="form-control shadow-none" 
+                    placeholder="Insert a programming language to learn" name="language" v-model="newLang">
+                    <button  class="btn btn-warning">Add</button>
+                </form>
+            </div>
+        </div>
+        
     </div>
+    
+    
+</div>
 
 
 
-    <script src="js/main.js"></script>
+<script src="js/main.js"></script>
 </body>
 </html>
--- js/main.js
+++ js/main.js
@@ -4,6 +4,35 @@ const {createApp} = Vue;
 createApp({
     data(){
         return{
+            ListPL:[],
+            newLang:"",
+            params:{
+                name: null,
+                usedFor:"",
+                logo: "https://picsum.photos/200/300"
+            }
+
         }
+    },
+    methods:{
+        fetchLangList(){
+            axios.get("./api/programmingLanguageList.php")
+            .then(resp=>{
+                console.log(resp.data);
+                this.ListPL = resp.data;
+            })
+        },
+        insertNew(){
+
+            axios.post("./api/newPLang.php",this.params,{
+                headers:{"Content-Type":'multipart/form-data'}
+            })
+            .then(resp=>{
+                this.fetchLangList();
+            })
+        }
+    },
+    mounted(){
+        this.fetchLangList();
     }
 }).mount("#app")
